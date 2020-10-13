659f5819dacde9b5761f43d2b462391e19e7a5a5
Bryan Castro <49798419+Bratt-ec@users.noreply.github.com>
2020-10-13 12:16:45 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAtjUqxXLvxSbDwGAC7pdhhvmixUHmvS5M",
-    authDomain: "portafolio-4cef4.firebaseapp.com",
-    databaseURL: "https://portafolio-4cef4.firebaseio.com",
-    projectId: "portafolio-4cef4",
-    storageBucket: "portafolio-4cef4.appspot.com",
-    messagingSenderId: "113091747074",
-    appId: "1:113091747074:web:e6277dd8d9e7cb9833b61b",
-    measurementId: "G-SL2DW20J7K"
+    apiKey: " ",
+    authDomain: " ",
+    databaseURL: " ",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: " ",
+    measurementId: " "
   };
 
 // Initialize Firebase
